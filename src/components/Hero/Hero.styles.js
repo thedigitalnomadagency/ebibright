fb9f6102e3e7a299bf3413aea0460ec8529531c0
prettyboinerd <delasimensa@gmail.com>
2020-09-17 20:23:01 +0000

    updated hero section

--- src/components/Hero/Hero.styles.js
+++ src/components/Hero/Hero.styles.js
@@ -1,10 +1,55 @@
 import tw, { styled } from "twin.macro"
 
 export default styled.div`
-  ${tw`border border-black`}
-  height:60vh;
+  ${tw``}
+  height:40vh;
 
-  /* @media screen and (min-width: 480px) {
-   
-  } */
+  @media screen and (min-width: 768px) {
+    height: 80vh;
+  }
+
+  .inner-wrapper {
+    ${tw`container mx-auto  lg:flex h-full`}
+  }
+
+  .image-box {
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
+
+    @media screen and (min-width: 769px) {
+      width: 30%;
+      height: 100%;
+      padding-top: 50px;
+      padding-left: 15px;
+    }
+  }
+
+  .image {
+    width: 100%;
+  }
+
+  .message {
+    ${tw`flex flex-col justify-center px-8 `}
+
+    @media screen and (min-width: 769px) {
+      width: 70%;
+    }
+  }
+
+  .heading {
+    ${tw`lg:text-4xl lg:mb-3`}
+  }
+
+  .sub-1 {
+    color: #e4161d;
+  }
+
+  .sub-2 {
+    color: #000;
+  }
+
+  .sub-3 {
+    color: #096537;
+  }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 //components
 import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
+import Donations from "../components/DonationsSection/Donations"
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <Donations />
     </Layout>
   )
 }
